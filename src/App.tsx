6b8d934007e145832ee907b0f13464d258043e2a
Inokentii Mazhara <keshan3262@gmail.com>
2023-10-07 22:54:57 +0300

    Change attribute name

--- src/App.tsx
+++ src/App.tsx
@@ -84,7 +84,7 @@ const App = observer(() => {
   }, []);
 
   return (
-    <div test-id="App">
+    <div data-testid="App">
       <div className="row">
         <select value={selectedNetworkId} onChange={handleSelectedNetworkChange}>
           {Object.values(networks).map(({ id, fullName }) => (
